1ef3b6a56a5ae5f1271dd1efc751803153d643c4
silvericarus <agonzalezrosa@hotmail.com>
2023-06-27 00:06:30 +0200

    m1ex05

---
+++ Module01/ex05/Harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albgonza <albgonza@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/26 23:34:32 by albgonza          #+#    #+#             */
+/*   Updated: 2023/06/26 23:34:32 by albgonza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <map>
+
+class Harl
+{
+	typedef void (Harl::*ComplaintFunction)();
+    std::map<std::string, ComplaintFunction> complaints;
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+	public:
+		Harl();
+		~Harl();
+		void	complain(std::string level);
+
+};
