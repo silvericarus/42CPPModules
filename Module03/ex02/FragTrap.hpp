e5b94d945e459906be2836b2adb8021a06e6e344
silvericarus <agonzalezrosa@hotmail.com>
2023-07-11 21:00:36 +0200

    m3ex02 done

---
+++ Module03/ex02/FragTrap.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albgonza <albgonza@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/11 20:51:51 by albgonza          #+#    #+#             */
+/*   Updated: 2023/07/11 20:51:51 by albgonza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+
+class FragTrap : public ClapTrap
+{
+	private:
+		std::string	_name;
+		int			_hp;
+		int			_ep;
+		int			_damage;
+	public:
+		FragTrap(std::string name);
+		~FragTrap();
+		void	attack(const std::string& target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+		void	highFivesGuys(void);
+};
+
+#endif
